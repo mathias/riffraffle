2200c7792fa41f3bb4e5599102ad1a7274d077bf
Matt Gauger <matt.gauger@gmail.com>
2013-02-08 18:10:21 -0600

    Initial commit new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnowMobileRaffle::Application.config.secret_token = '59ad125458337a2ac0803e733f67386d41e8f71e02cc60cb82631330cb1e2f56c99738615a2966a43e49be3c880a15a3f9f3aea2cc90502d6d032f0fb0ddcd8a'
